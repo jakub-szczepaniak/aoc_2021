223fafb26e705149c394ef4276e1ce3d15d16c82
unknown <szczepaniak.jkb@gmail.com>
2021-12-01 20:45:33 +0100

    Day 1. Input + first test.

---
+++ src/day_1.js
@@ -0,0 +1,9 @@
+
+function part_1(input) {
+    return 0;
+
+}
+
+module.exports = {
+    part_1: part_1
+}
---
+++ test/day_1.test.js
@@ -0,0 +1,6 @@
+const day_1 = require('../src/day_1.js');
+
+test("works", function(){
+    let input = ["123"];
+    expect(day_1.part_1(input)).toBe(0);
+});

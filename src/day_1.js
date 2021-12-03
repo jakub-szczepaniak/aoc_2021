00d2a79f0710abc4e3f1650adc0681fdfd8a8e96
unknown <szczepaniak.jkb@gmail.com>
2021-12-03 21:55:53 +0100

    Refactored test and code to test through whole module.

--- src/day_1.js
+++ src/day_1.js
@@ -1,9 +1,38 @@
+const fs = require('fs');
+const path = require('path');
 
-function part_1(input) {
-    return 0;
+class SubmarineSonar {
+    static build_from() {
+        if((arguments.length == 1) && arguments[0].endsWith('input')) {
+            return input(arguments[0]);
+        } else {
+            return arguments;
+        }
+    }
+    input(filename) {
 
+        var text = fs.readFileSync(filename).toString();
+        return text.split("\r\n")
+    }
 }
 
+class SonarAnalyzer {
+    depth(input) {
+        if (input.length <= 1) {
+            return 0;
+        }
+        let result = 0;
+        for(let i = 1; i< input.length; i++) {
+            if (input[i-1]<input[i]) {
+                result++;
+            }  
+        }
+        return result;
+    }
+}
+
+
 module.exports = {
-    part_1: part_1
+    SonarAnalyzer: SonarAnalyzer,
+    SubmarineSonar: SubmarineSonar
 }
--- test/day_1.test.js
+++ test/day_1.test.js
@@ -1,6 +1,19 @@
 const day_1 = require('../src/day_1.js');
 
-test("works", function(){
-    let input = ["123"];
-    expect(day_1.part_1(input)).toBe(0);
+test("for single element, returns 0", function(){
+    readings = day_1.SubmarineSonar.build_from("123");
+    subject = new day_1.SonarAnalyzer();
+    expect(subject.depth(readings)).toBe(0);
 });
+
+test("for 2 elements, if first is lower than 2nd, return 1", function(){
+    readings = day_1.SubmarineSonar.build_from("123", "234");
+    subject = new day_1.SonarAnalyzer();
+    expect(subject.depth(readings)).toBe(1);
+});
+
+test.skip("For big readings returns solution value", function (){
+    readings = day_1.SubmarineSonar.build_from(__dirname + "/day_1.input");
+    subject = new day_1.SonarAnalyzer();
+    expect(subject.depth(readings)).toBe(42);
+}) 

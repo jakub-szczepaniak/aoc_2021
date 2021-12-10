57d68f7f0bd39e218d342445cdb1d808bcf6ef53
unknown <szczepaniak.jkb@gmail.com>
2021-12-10 21:56:35 +0100

    Moves tests around.

--- test/day_1.test.js
+++ test/day_1.test.js
@@ -3,15 +3,16 @@ const each = require('jest-each').default;
 const day_1 = require('../src/day_1.js');
 
 describe("Sonar analyzer", function(){
+    test("for empty report, throw an error", function(){
+        report = day_1.SubmarineSonar.build_report([]);
+        subject = new day_1.SonarAnalyzer();
+        expect(() => { subject.analyze(report) } ).toThrow();
+    });
     test("for single measurement, flags there is no previous measurement", function(){
         report = day_1.SubmarineSonar.build_report(["123"]);
         subject = new day_1.SonarAnalyzer();
         expect(subject.analyze(report)).toBe("N/A - no previous measurement");
     });
 
-    test("for empty report, throw an error", function(){
-        report = day_1.SubmarineSonar.build_report([]);
-        subject = new day_1.SonarAnalyzer();
-        expect(() => { subject.analyze(report) } ).toThrow();
-    })
+    
 });

de0de6abae7fe1c3d58298bc648b26c7e9a6e930
unknown <szczepaniak.jkb@gmail.com>
2021-12-10 21:48:19 +0100

    Enclosed tests in the describe block.

--- test/day_1.test.js
+++ test/day_1.test.js
@@ -1,16 +1,17 @@
-const { it } = require('jest-circus');
+const { it, describe } = require('jest-circus');
 const each = require('jest-each').default;
 const day_1 = require('../src/day_1.js');
 
-test("for single measurement, flags there is no previous measurement", function(){
-    report = day_1.SubmarineSonar.build_report(["123"]);
-    subject = new day_1.SonarAnalyzer();
-    expect(subject.analyze(report)).toBe("N/A - no previous measurement");
+describe("Sonar analyzer", function(){
+    test("for single measurement, flags there is no previous measurement", function(){
+        report = day_1.SubmarineSonar.build_report(["123"]);
+        subject = new day_1.SonarAnalyzer();
+        expect(subject.analyze(report)).toBe("N/A - no previous measurement");
+    });
+
+    test("for empty report, throw an error", function(){
+        report = day_1.SubmarineSonar.build_report([]);
+        subject = new day_1.SonarAnalyzer();
+        expect(() => { subject.analyze(report) } ).toThrow();
+    })
 });
-
-test("for empty report, throw an error", function(){
-    report = day_1.SubmarineSonar.build_report([]);
-    subject = new day_1.SonarAnalyzer();
-    expect(() => { subject.analyze(report) } ).toThrow();
-})
-

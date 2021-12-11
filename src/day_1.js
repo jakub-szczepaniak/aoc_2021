fb02c6967e23d924d30d4a99a5faa8bc7222aeed
unknown <szczepaniak.jkb@gmail.com>
2021-12-11 22:23:48 +0100

    Day 1 part 1 solved.

--- src/day_1.js
+++ src/day_1.js
@@ -3,7 +3,11 @@ const path = require('path');
 
 class SubmarineSonar {
     static build_report(readings){
-        return readings;
+        return readings.map((reading) => {return Number(reading)});
+    }
+    static read_from(filepath) {
+        const data = fs.readFileSync(filepath, 'utf8')
+        return this.build_report(data.split("\r\n"));
     }
 }
 
@@ -12,7 +16,11 @@ class SonarAnalyzer {
         if (report.length < 1) { throw SonarFailure;}
         if (report.length == 1) { return this.no_previous_readings();}
         if (report.length > 1) {
-            return this.count_increases(report[0], report[1]);
+            var increases = 0;
+            for(var i=1; i<=report.length; i++) {
+                increases = increases + this.count_increases(report[i-1], report[i]);
+            }
+            return increases;
         }
     }
     count_increases(reading_1, reading_2) {
--- test/day_1.test.js
+++ test/day_1.test.js
@@ -7,26 +7,43 @@ describe("Sonar analyzer", function(){
         expect(() => { subject.analyze(report) } ).toThrow();
     });
     test("for single measurement, flags there is no previous measurement", function(){
-        report = make_report(["123"]);;
+        report = make_report(["0"]);;
         subject = new day_1.SonarAnalyzer();
         expect(subject.analyze(report)).toBe("N/A - no previous measurement");
     });
-    test("two equal statements -> 0 increase", function(){
-        report = make_report(["123", "123"]);
+    test("two equal readings -> 0 increase", function(){
+        report = make_report(["1", "1"]);
         subject = new day_1.SonarAnalyzer();
         expect(subject.analyze(report)).toBe(0);
     });
-    test("two different statements -> 1 increase", function(){
-        report = make_report(["123", "124"]);
+    test("two different readings -> 1 increase", function(){
+        report = make_report(["1", "2"]);
         subject = new day_1.SonarAnalyzer();
         expect(subject.analyze(report)).toBe(1);
     });
 
-    test("two different statements -> 0 increase", function(){
-        report = make_report(["124", "123"]);
+    test("two different readings -> 0 increase", function(){
+        report = make_report(["2", "1"]);
         subject = new day_1.SonarAnalyzer();
         expect(subject.analyze(report)).toBe(0);
     });
+
+    test("2 same readings, 1 bigger -> 1 increase", function(){
+        report = make_report(["1", "1", "2"]);
+        subject = new day_1.SonarAnalyzer();
+        expect(subject.analyze(report)).toBe(1);
+    });
+    test("3 different readings -> no increase", function(){
+        report = make_report(["3", "2", "1"]);
+        subject = new day_1.SonarAnalyzer();
+        expect(subject.analyze(report)).toBe(0);
+    });
+    test("for real input", function(){
+        report = make_report_from_file(__dirname + '/day_1.input');
+        subject = new day_1.SonarAnalyzer();
+        expect(subject.analyze(report)).toBe(1215);
+    })
+
 });
 
 function make_report(readings) {
@@ -35,4 +52,7 @@ function make_report(readings) {
 
 function make_empty_report() {
     return day_1.SubmarineSonar.build_report([]);
+}
+function make_report_from_file(filename) {
+    return day_1.SubmarineSonar.read_from(filename);
 }

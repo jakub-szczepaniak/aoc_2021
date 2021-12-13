5cab5ca0d0af573abd4c699d3838412aaf15aa52
unknown <szczepaniak.jkb@gmail.com>
2021-12-13 20:11:15 +0100

    We can now plot the course also vertically. Day 2.

--- test/day_2.test.js
+++ test/day_2.test.js
@@ -6,13 +6,21 @@ describe("Submarine course plotter", function(){
    course_plot.move("forward 1");
    expect(course_plot.horizontal).toBe(1);
   });
+  test("we add command down 1 then vertical position is 1", function(){
+    let course_plot = new CoursePlotter();
+    course_plot.move("down 1");
+    expect(course_plot.vertical).toBe(1);
+  })
+
 });
 
 class CoursePlotter{
   constructor(){
     this.horizontal = 0;
+    this.vertical = 0;
   }
   move(command){
     this.horizontal = 1;
+    this.vertical = 1;
   }
 }

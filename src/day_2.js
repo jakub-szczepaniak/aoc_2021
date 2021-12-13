6ea865bcbb3571fe4daf0eb69efab9f902b9ca4a
unknown <szczepaniak.jkb@gmail.com>
2021-12-13 20:14:13 +0100

    Moves the Course plotter to separate module. Day 2.

--- src/day_2.js
+++ src/day_2.js
@@ -1,2 +1,14 @@
+class CoursePlotter{
+  constructor(){
+    this.horizontal = 0;
+    this.vertical = 0;
+  }
+  move(command){
+    this.horizontal = 1;
+    this.vertical = 1;
+  }
+}
+
 module.exports = {
+  CoursePlotter: CoursePlotter
 }
--- test/day_2.test.js
+++ test/day_2.test.js
@@ -2,25 +2,18 @@ const day_2 = require('../src/day_2.js');
 
 describe("Submarine course plotter", function(){
   test("we add command forward 1 then the horizontal position is 1", function(){
-   let course_plot = new CoursePlotter();
+   let course_plot = make_plotter();
    course_plot.move("forward 1");
    expect(course_plot.horizontal).toBe(1);
   });
   test("we add command down 1 then vertical position is 1", function(){
-    let course_plot = new CoursePlotter();
+    let course_plot = make_plotter();
     course_plot.move("down 1");
     expect(course_plot.vertical).toBe(1);
   })
 
 });
 
-class CoursePlotter{
-  constructor(){
-    this.horizontal = 0;
-    this.vertical = 0;
-  }
-  move(command){
-    this.horizontal = 1;
-    this.vertical = 1;
-  }
+function make_plotter() {
+  return new day_2.CoursePlotter();
 }

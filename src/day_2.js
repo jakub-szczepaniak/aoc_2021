ec8e482c3a27370c0a1c91a6283f1b62838cd476
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2021-12-15 22:54:54 +0100

    Extracts class to hold movement commands. Day 2.

--- src/day_2.js
+++ src/day_2.js
@@ -3,11 +3,13 @@ class CoursePlotter{
     this.horizontal = 0;
     this.vertical = 0;
   }
-  move(command){
-    switch (command) {
-      case 'forward 1':
-        this.horizontal = 1;
+  move(command, movement=""){
+    switch (movement.direction) {
+      case 'forward':
+        this.horizontal = this.horizontal + movement.value;
         break;
+      }
+    switch (command) {
       case 'down 1': 
         this.vertical = this.vertical + 1;
         break;
@@ -20,6 +22,14 @@ class CoursePlotter{
   }
 }
 
+class Command{
+  constructor(direction, value) {
+    this.direction = direction;
+    this.value = value;
+  }
+}
+
 module.exports = {
-  CoursePlotter: CoursePlotter
+  CoursePlotter: CoursePlotter,
+  Command: Command
 }
--- test/day_2.test.js
+++ test/day_2.test.js
@@ -3,14 +3,15 @@ const day_2 = require('../src/day_2.js');
 describe("Submarine course plotter", function(){
   test("we add command 'forward 1' then the horizontal position is 1", function(){
    let course_plot = make_plotter();
-   course_plot.move("forward 1");
+   let command = move_forward(1);
+   course_plot.move("forward 1", command);
    expect(course_plot.horizontal).toBe(1);
   });
   test("we add command 'down 1' then vertical position is 1", function(){
     let course_plot = make_plotter();
     course_plot.move("down 1");
     expect(course_plot.vertical).toBe(1);
-  })
+  });
 
   test("we add command 'down 1' then 'up 1'  thern the vertical position is 0", function(){
     let course_plot = make_plotter();
@@ -18,10 +19,23 @@ describe("Submarine course plotter", function(){
     expect(course_plot.vertical).toBe(1);
     course_plot.move("up 1");
     expect(course_plot.vertical).toBe(0);
-  })
+  });
+
+  test("we add command forward twice -> then horizontal position should be 2", function(){
+    let course_plot = make_plotter();
+    let command = move_forward(1);
+    course_plot.move("forward 1", command);
+    course_plot.move("forward 1", command);
+    expect(course_plot.horizontal).toBe(2);
+  });
 
 });
 
 function make_plotter() {
   return new day_2.CoursePlotter();
 }
+
+function move_forward(value) {
+  return new day_2.Command("forward", value);
+}
+

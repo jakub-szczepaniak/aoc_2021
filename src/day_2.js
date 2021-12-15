febd7e3a3814510685464b86a410413ce649e540
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2021-12-15 23:02:28 +0100

    Removes handler based on text name of command. Day 2.

--- src/day_2.js
+++ src/day_2.js
@@ -3,20 +3,16 @@ class CoursePlotter{
     this.horizontal = 0;
     this.vertical = 0;
   }
-  move(command, movement=""){
+  move(movement=""){
     switch (movement.direction) {
       case 'forward':
         this.horizontal = this.horizontal + movement.value;
         break;
       case 'down':
         this.vertical = this.vertical + movement.value;
-      }
-    switch (command) {
-      case 'up 1':
-        this.vertical = this.vertical - 1;
         break;
-      default:
-        break;
+      case 'up':
+        this.vertical = this.vertical - movement.value;
     }
   }
 }
--- test/day_2.test.js
+++ test/day_2.test.js
@@ -4,30 +4,31 @@ describe("Submarine course plotter", function(){
   test("we add command 'forward 1' then the horizontal position is 1", function(){
    let course_plot = make_plotter();
    let command = move_forward(1);
-   course_plot.move("forward 1", command);
+   course_plot.move(command);
    expect(course_plot.horizontal).toBe(1);
   });
   test("we add command 'down 1' then vertical position is 1", function(){
     let course_plot = make_plotter();
     let command = move_down(1);
-    course_plot.move("down 1", command);
+    course_plot.move(command);
     expect(course_plot.vertical).toBe(1);
   });
 
   test("we add command 'down 1' then 'up 1'  thern the vertical position is 0", function(){
     let course_plot = make_plotter();
     let command = move_down(1);
-    course_plot.move("down 1", command);
+    let command_up = move_up(1);
+    course_plot.move(command);
     expect(course_plot.vertical).toBe(1);
-    course_plot.move("up 1");
+    course_plot.move(command_up);
     expect(course_plot.vertical).toBe(0);
   });
 
   test("we add command forward twice -> then horizontal position should be 2", function(){
     let course_plot = make_plotter();
     let command = move_forward(1);
-    course_plot.move("forward 1", command);
-    course_plot.move("forward 1", command);
+    course_plot.move(command);
+    course_plot.move(command);
     expect(course_plot.horizontal).toBe(2);
   });
 
@@ -43,5 +44,9 @@ function move_forward(value) {
 
 function move_down(value) {
   return new day_2.Command("down", value);
+}
+
+function move_up(value) {
+  return new day_2.Command("up", value);
 }
 

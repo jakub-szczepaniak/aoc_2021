f9619d448993bb10db7ceb1741db8e16c0570840
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2021-12-19 21:20:10 +0100

    Do not test via method #move but through the method to run a list of commands.

--- test/day_2.test.js
+++ test/day_2.test.js
@@ -3,39 +3,32 @@ const day_2 = require('../src/day_2.js');
 describe("Submarine course plotter", function(){
   test("we add command 'forward 1' then the horizontal position is 1", function(){
    let course_plot = make_plotter();
-   let command = move_forward(1);
-   course_plot.move(command);
+   
+   course_plot.move_many([move_forward(1)]);
+   
    expect(course_plot.horizontal).toBe(1);
   });
   test("we add command 'down 1' then vertical position is 1", function(){
     let course_plot = make_plotter();
-    let command = move_down(1);
-    course_plot.move(command);
+    
+    course_plot.move_many([move_down(1)]);
+    
     expect(course_plot.vertical).toBe(1);
   });
 
   test("we add command 'down 1' then 'up 1'  thern the vertical position is 0", function(){
     let course_plot = make_plotter();
-    let command = move_down(1);
-    let command_up = move_up(1);
-    course_plot.move(command);
-    expect(course_plot.vertical).toBe(1);
-    course_plot.move(command_up);
+    
+    course_plot.move_many([move_down(1), move_up(1)]);
+    
     expect(course_plot.vertical).toBe(0);
   });
 
-  test("we add command forward twice -> then horizontal position should be 2", function(){
+  test("We can pass command forward twice commands as a list", function(){
     let course_plot = make_plotter();
-    let command = move_forward(1);
-    course_plot.move(command);
-    course_plot.move(command);
-    expect(course_plot.horizontal).toBe(2);
-  });
-
-  test("We can pass many commands as a list", function(){
-    let course_plot = make_plotter();
-    let commands = [move_forward(1), move_forward(2)];
-    course_plot.move_many(commands);
+    
+    course_plot.move_many([move_forward(1), move_forward(2)]);
+    
     expect(course_plot.horizontal).toBe(3);
   })
 

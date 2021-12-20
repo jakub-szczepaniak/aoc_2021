416dc793818d1853a126220bd8a1cd93a23a54b3
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2021-12-20 22:08:11 +0100

    Verify the multiply output instead of single coordinates. Day 2.

--- test/day_2.test.js
+++ test/day_2.test.js
@@ -2,42 +2,37 @@ const day_2 = require('../src/day_2.js');
 const {tags} = require('jest-tags');
 
 describe("Submarine course plotter", function(){
-  test("we add command 'forward 1' then the horizontal position is 1", function(){
+  test("after command 'forward 1', multiplier is 0", function(){
    let course_plot = make_plotter();
    
    course_plot.move_many([move_forward(1)]);
    
-   expect(course_plot.horizontal).toBe(1);
+   expect(course_plot.multiplier()).toBe(0);
   });
-  test("we add command 'down 1' then vertical position is 1", function(){
+  test("after command 'down 1', multiplier is 0", function(){
     let course_plot = make_plotter();
     
     course_plot.move_many([move_down(1)]);
     
-    expect(course_plot.vertical).toBe(1);
+    expect(course_plot.multiplier()).toBe(0);
   });
 
-  test("we add command 'down 1' then 'up 1'  thern the vertical position is 0", function(){
+  test("after commands 'forward 1', 'down 2', multiplier is 2", function(){
     let course_plot = make_plotter();
     
-    course_plot.move_many([move_down(1), move_up(1)]);
+    course_plot.move_many([move_forward(1), move_down(2)]);
     
-    expect(course_plot.vertical).toBe(0);
+    expect(course_plot.multiplier()).toBe(2);
   });
 
-  test("We can pass command forward twice commands as a list", function(){
+  test("after commands 'forward 1', 'forward 2', mutliplier is 0", function(){
     let course_plot = make_plotter();
     
     course_plot.move_many([move_forward(1), move_forward(2)]);
     
-    expect(course_plot.horizontal).toBe(3);
+    expect(course_plot.multiplier()).toBe(0);
   })
-  test("We can pass a list of commands as a string and calculate", function(){
-    let course_plot = make_plotter();
-    let course = day_2.CourseParser.build_course(["forward 1", "forward 2", "forward 3"]);
-    course_plot.move_many(course)
-    expect(course_plot.horizontal).toBe(6);
-  })
+
   tags("integration").test("Test for the input file", function(){
     let course_plot = make_plotter();
     let course = day_2.CourseParser.load_from_file(__dirname + '/day_2.input')
